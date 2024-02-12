d1259da36a9c6ce70f4468b501ecd60ab2188ccc
ajianggo <ajianggo@gmail.com>
2024-02-12 21:07:56 +0700

    Firsh Commit Search Product

---
+++ Project WEB/Kelompok_3/Object Repository/Secondhand Website/Search Product - Aji/Latihan Commit.rs
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Latihan Commit</name>
+   <tag></tag>
+   <elementGuidId>80ecc4e9-4007-4451-984b-d754b274fe1e</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
