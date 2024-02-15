8cc0260e2650679f38d79f012861ca13356ab323
shinjulyana <shintajuly.shinta@gmail.com>
2024-02-15 21:29:57 +0700

    coba

---
+++ Project WEB/Kelompok_3/Object Repository/Secondhand Website/Feature 1 - Shinta/Landing Page/Coba.rs
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Coba</name>
+   <tag></tag>
+   <elementGuidId>eab5437e-0011-4dcb-8c36-14c20d082315</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
