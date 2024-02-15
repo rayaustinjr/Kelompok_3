0c78c09d98f18518693fa81f9e4ee7040ca4e009
ajianggo <ajianggo@gmail.com>
2024-02-15 21:49:08 +0700

    Buat Folder

---
+++ Project WEB/Kelompok_3/Object Repository/Secondhand Website/Feature 5 - Aji/Produk Diminati/New Test Object.rs
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>New Test Object</name>
+   <tag></tag>
+   <elementGuidId>39635975-3d7c-4fe4-b219-488858437703</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
