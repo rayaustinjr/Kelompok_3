0f81dec0a97470da3771e8843fb007f5df56fb02
ajianggo <ajianggo@gmail.com>
2024-02-16 21:45:05 +0700

    Update Search Product

---
+++ Project WEB/Kelompok_3/Object Repository/Secondhand Website/Feature 5 - Aji/Search Product/Colom_Search.rs
@@ -0,0 +1,67 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Colom_Search</name>
+   <tag></tag>
+   <elementGuidId>dc29fcd3-0b0a-4910-bc95-9e3b0997b609</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@id='navbarSupportedContent']/form/div</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>div.input-group.rounded-pill.bg-light.p-1.search</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>div</value>
+      <webElementGuid>818d07a0-6b39-4e35-9e9d-bc72e8f592e4</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>input-group rounded-pill bg-light p-1 search</value>
+      <webElementGuid>c7e885c3-bd68-45b8-b5fe-c2f5d18d861b</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;navbarSupportedContent&quot;)/form[@class=&quot;d-flex ms-0 ms-lg-4 my-4 my-lg-0&quot;]/div[@class=&quot;input-group rounded-pill bg-light p-1 search&quot;]</value>
+      <webElementGuid>9c44f829-9d4c-4b9a-9708-cd0574b3a618</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='navbarSupportedContent']/form/div</value>
+      <webElementGuid>2a63d503-7bc5-48f1-b8dc-2f3f5ea9f63e</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Bulan RamadhanBanyak diskon!'])[1]/preceding::div[3]</value>
+      <webElementGuid>5d46f3f0-6230-4982-8e45-22f167112f4c</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//form/div</value>
+      <webElementGuid>490dbac4-6c4a-4280-9ed2-8c2ffe664f94</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
