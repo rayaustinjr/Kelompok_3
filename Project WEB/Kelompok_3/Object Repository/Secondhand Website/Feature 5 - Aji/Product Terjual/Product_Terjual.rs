bb0d798aaecee98fd91f9228467ba666ac8273ee
ajianggo <ajianggo@gmail.com>
2024-02-16 21:09:36 +0700

    ID03_Aji Product Terjual Object Repo

---
+++ Project WEB/Kelompok_3/Object Repository/Secondhand Website/Feature 5 - Aji/Product Terjual/Product_Terjual.rs
@@ -0,0 +1,95 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Product_Terjual</name>
+   <tag></tag>
+   <elementGuidId>bf802638-a1af-4dbe-844e-413b5ace1a80</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>(.//*[normalize-space(text()) and normalize-space(.)='Terjual'])[1]/following::div[5]</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>div.card-body.text-decoration-none</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>div</value>
+      <webElementGuid>c3e89b0d-804c-4415-979b-cfe27f9563a1</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>card-body text-decoration-none</value>
+      <webElementGuid>c21ce5a3-4fbf-44f5-8a4c-5ed3b0e4495e</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+      Aaaa
+      Kendaraan
+      Rp 23
+    </value>
+      <webElementGuid>fc00948a-f345-4929-87e6-4a946e47b0d1</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>/html[1]/body[1]/section[@class=&quot;pt-5 mt-5&quot;]/div[@class=&quot;container&quot;]/div[@class=&quot;row my-5&quot;]/div[@class=&quot;col-12 col-lg-9 mt-5 mt-lg-0&quot;]/div[@class=&quot;row g-4&quot;]/div[@class=&quot;col-12 col-lg-4&quot;]/a[@class=&quot;product-card&quot;]/div[@class=&quot;card px-0 border-0 shadow h-100 pb-4 rounded-4&quot;]/div[@class=&quot;card-body text-decoration-none&quot;]</value>
+      <webElementGuid>4f8dcfe3-efde-4517-90aa-d83e614d04db</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Terjual'])[1]/following::div[5]</value>
+      <webElementGuid>2c5d36bb-5d72-49bd-ac2b-e2242eba5f0b</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Diminati'])[1]/following::div[5]</value>
+      <webElementGuid>35ae36a5-f33e-44fd-9adc-99c273ee426a</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div/a/div/div</value>
+      <webElementGuid>1309b674-6868-479e-b38e-421baf934e06</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//div[(text() = '
+      Aaaa
+      Kendaraan
+      Rp 23
+    ' or . = '
+      Aaaa
+      Kendaraan
+      Rp 23
+    ')]</value>
+      <webElementGuid>ad7fcc57-a471-498e-93f3-118bbad587c5</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
