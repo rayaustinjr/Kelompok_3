4d3f973a47c2675a5178daa1e3e94af1bc7424ea
mariabinar <mariatigultom96369@gmail.com>
2024-02-26 20:31:09 +0700

    penambahan card product di folder offer product

---
+++ Project WEB/Kelompok_3/Object Repository/Page_SecondHand/card product.rs
@@ -0,0 +1,99 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>card product</name>
+   <tag></tag>
+   <elementGuidId>1c37c7cb-b5bd-4bd5-968b-cbe5234f569a</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@id='products']/div/a/div</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>div.card.px-0.border-0.shadow.h-100.pb-4.rounded-4</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>div</value>
+      <webElementGuid>f2595247-e5c5-48ba-8fd1-98545d6d537d</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>card px-0 border-0 shadow h-100 pb-4 rounded-4</value>
+      <webElementGuid>a52a9352-bea7-4b58-af0c-771714f192d1</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+      
+
+    
+      CBkvt
+      Elektronik
+      Rp 1
+    
+  </value>
+      <webElementGuid>ec5ec46d-d818-4d90-94b9-e02b2b429bdd</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;products&quot;)/div[@class=&quot;col-12 col-md-4 col-lg-3&quot;]/a[@class=&quot;product-card&quot;]/div[@class=&quot;card px-0 border-0 shadow h-100 pb-4 rounded-4&quot;]</value>
+      <webElementGuid>a30910ac-a0f3-46c0-9cde-ee35f820e31e</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='products']/div/a/div</value>
+      <webElementGuid>4946fade-a43b-46dc-bc3b-12eb512e3278</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//a/div</value>
+      <webElementGuid>5ffc254b-afd5-4145-a3e8-027d0ba571eb</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//div[(text() = '
+      
+
+    
+      CBkvt
+      Elektronik
+      Rp 1
+    
+  ' or . = '
+      
+
+    
+      CBkvt
+      Elektronik
+      Rp 1
+    
+  ')]</value>
+      <webElementGuid>8b3f9569-fa8f-4aec-a096-6686050536ed</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
