d4f3864d5e03f60b808f791127fc8967c6a4550a
shinjulyana <shintajuly.shinta@gmail.com>
2024-02-28 08:19:07 +0700

    Melengkapi Step Definition TC Login LG004-LG008

--- Project WEB/Kelompok_3/Scripts/Pages/Feature 1 - Shinta/Login/Verify Password Required/Script1709048019199.groovy
+++ Project WEB/Kelompok_3/Scripts/Pages/Feature 1 - Shinta/Login/Verify Password Required/Script1709048019199.groovy
@@ -17,15 +17,8 @@ import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
-WebUI.callTestCase(findTestCase('Pages/Feature 1 - Shinta/Login/Click Button Masuk'), [:], FailureHandling.STOP_ON_FAILURE)
+message = WebUI.getAttribute(findTestObject('Secondhand Website/Feature 1 - Shinta/Login Page/input_Password_userpassword'), 
+    'validationMessage')
 
-WebUI.callTestCase(findTestCase('Pages/Feature 1 - Shinta/Login/Verify Content'), [:], FailureHandling.STOP_ON_FAILURE)
-
-WebUI.callTestCase(findTestCase('Pages/Feature 1 - Shinta/Login/Input Registered Email'), [('email') : 'try@mail.com'], 
-    FailureHandling.STOP_ON_FAILURE)
-
-WebUI.callTestCase(findTestCase('Pages/Feature 1 - Shinta/Login/Click_btn_Login'), [:], FailureHandling.STOP_ON_FAILURE)
-
-WebUI.callTestCase(findTestCase('null'), [('message') : 'Please fill out this field.'
-        , ('expected') : [('') : message]], FailureHandling.STOP_ON_FAILURE)
+WebUI.verifyEqual(message, expected)
 
