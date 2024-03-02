af1a0df2c66082c84374a125226f6a6affaf5077
mariati88 <151051257+mariati88@users.noreply.github.com>
2024-03-02 09:27:23 +0700

    add obeject repo untuk btn waiting respon dan status penawaran

---
+++ Project WEB/Kelompok_3/Object Repository/Secondhand Website/Feature 4 - Mariati/AcceptOffer/Status Penawaran.rs
@@ -0,0 +1,105 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Status Penawaran</name>
+   <tag></tag>
+   <elementGuidId>d21d02e9-6092-4b95-9ef0-dcc40928c51b</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>(//section//h6[normalize-space(text())='${status}'])[1]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>h6</value>
+      <webElementGuid>8664424a-0426-42df-beac-50180e488048</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>offer-status text-black-50</value>
+      <webElementGuid>b583c91a-89b8-48bf-92df-d0af5f887f7b</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+              Penawaran produk diterima
+            </value>
+      <webElementGuid>f644768d-c679-4133-8622-dd2f44e9b9ba</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>/html[1]/body[1]/section[@class=&quot;pt-5 mt-5&quot;]/section[@class=&quot;container-sm&quot;]/ul[@class=&quot;offers mt-5&quot;]/li[@class=&quot;offer gap-4 mt-5&quot;]/div[@class=&quot;offer-content&quot;]/div[@class=&quot;offer-header&quot;]/h6[@class=&quot;offer-status text-black-50&quot;]</value>
+      <webElementGuid>df6cffc5-2488-4222-89fa-46d074de453e</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Batalkan transaksi'])[1]/following::h6[1]</value>
+      <webElementGuid>0fe67d7c-12c3-40dc-b24c-0c727fd0a226</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Berhasil terjual'])[1]/following::h6[1]</value>
+      <webElementGuid>c0c1b0c9-ddf2-491d-bd9b-b37333fe5c66</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='sayang'])[2]/preceding::h6[1]</value>
+      <webElementGuid>e1c95f1e-4bd5-476a-98df-31a9deab0d18</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Rp 2.000'])[1]/preceding::h6[1]</value>
+      <webElementGuid>c85805a4-85ff-4b3b-92cf-d80eb0662366</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//li[3]/div/div/h6</value>
+      <webElementGuid>02578bea-309b-4b5a-a5f9-613a7176959b</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//h6[(text() = '
+              Penawaran produk diterima
+            ' or . = '
+              Penawaran produk diterima
+            ')]</value>
+      <webElementGuid>0bdc0064-8849-43c7-bdfe-a48b599206b7</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
