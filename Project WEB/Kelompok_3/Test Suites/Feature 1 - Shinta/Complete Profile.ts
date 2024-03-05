d9b3c94dd579bc73408dc9ca196a3858fab86dd9
shinjulyana <shintajuly.shinta@gmail.com>
2024-03-06 03:00:34 +0700

    Complete Profile Step Def TC CP002-006 ok

---
+++ Project WEB/Kelompok_3/Test Suites/Feature 1 - Shinta/Complete Profile.ts
@@ -0,0 +1,56 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Complete Profile</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>120</pageLoadTimeout>
+   <pageLoadTimeoutDefault>false</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>20c03baa-7843-4ab2-bd79-d136409fae58</testSuiteGuid>
+   <testCaseLink>
+      <guid>e61d2342-c9c7-4c2f-983b-67d2ce337628</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Step Definition/Feature 1 - Shinta/Complete_Profile/CP001 - User successful to complete profile by fill every field and upload Profile Picture</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>89afab55-6cb7-42eb-b9eb-e299d4b0c90c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Step Definition/Feature 1 - Shinta/Complete_Profile/CP002 - User want to complete profile with empty address</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>0978a558-64c9-4387-9c1f-f9c5ac3ff5a6</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Step Definition/Feature 1 - Shinta/Complete_Profile/CP003 - User want to complete profile with empty phone number</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>649dd27e-f67a-4480-a3ee-ed267b7a3042</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Step Definition/Feature 1 - Shinta/Complete_Profile/CP004 - User want to complete profile by using caracter on phone number</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b488b599-c871-4183-b36e-0e7386bac140</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Step Definition/Feature 1 - Shinta/Complete_Profile/CP005 - User want to complete profile by not select user city</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>69268a1f-8888-4382-a007-555e634a9560</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Step Definition/Feature 1 - Shinta/Complete_Profile/CP006 - User want to complete profile without upload photo profile</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
