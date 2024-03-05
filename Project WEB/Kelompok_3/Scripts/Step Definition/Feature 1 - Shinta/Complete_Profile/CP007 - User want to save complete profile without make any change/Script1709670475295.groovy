39a15d4ef9a84548bf8c68c7e4f23cc670905df7
shinjulyana <shintajuly.shinta@gmail.com>
2024-03-06 03:36:00 +0700

    Menambah step definiton CP007 OK

---
+++ Project WEB/Kelompok_3/Scripts/Step Definition/Feature 1 - Shinta/Complete_Profile/CP007 - User want to save complete profile without make any change/Script1709670475295.groovy
@@ -0,0 +1,36 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+import org.apache.commons.lang3.RandomStringUtils as RandomStringUtils
+
+WebUI.callTestCase(findTestCase('Step Definition/Feature 1 - Shinta/Register/REG001 - Register with fresh account'), [:], 
+    FailureHandling.STOP_ON_FAILURE)
+
+WebUI.callTestCase(findTestCase('Pages/Feature 1 - Shinta/Complete Profile/Click Icon Account and Username'), [:], FailureHandling.STOP_ON_FAILURE)
+
+String[] City = ['Solo', 'Jogja', 'Jakarta', 'Bandung', 'Semarang']
+
+String address = (RandomStringUtils.randomAlphabetic(12) + ' No. ') + RandomStringUtils.randomNumeric(3)
+
+String NoHP = RandomStringUtils.randomNumeric(12)
+
+WebUI.callTestCase(findTestCase('Pages/Feature 1 - Shinta/Complete Profile/Click Save Button'), [:], FailureHandling.STOP_ON_FAILURE)
+
+WebUI.callTestCase(findTestCase('Pages/Feature 1 - Shinta/Complete Profile/Verify Select City'), [('message') : '', ('expected') : 'Please select an item in the list.'], 
+    FailureHandling.STOP_ON_FAILURE)
+
