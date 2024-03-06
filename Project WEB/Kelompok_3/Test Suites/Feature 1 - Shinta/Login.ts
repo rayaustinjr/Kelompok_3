9d4ab9358b259dcf745067732331f0310cb8ef31
shinjulyana <shintajuly.shinta@gmail.com>
2024-03-06 22:50:57 +0700

    Test Suites Feature 1 ok

--- Project WEB/Kelompok_3/Test Suites/Feature 1 - Shinta/Login.ts
+++ Project WEB/Kelompok_3/Test Suites/Feature 1 - Shinta/Login.ts
@@ -12,59 +12,59 @@
    <rerunImmediately>false</rerunImmediately>
    <testSuiteGuid>22080a9b-c4f2-4a2e-b387-fc6615f93c9b</testSuiteGuid>
    <testCaseLink>
-      <guid>89da8ce4-fe74-41dc-8092-6d14ac344d67</guid>
+      <guid>14762857-bddb-49a0-bf33-b519a186bc20</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Step Definition/Feature 1 - Shinta/Login/LG008 - User login without input email and password until user succsessful to login</testCaseId>
+      <testCaseId>Test Cases/Step Definition/Feature 1 - Shinta/Login/LG001 - User login with correct credential</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>743661bd-2000-40e5-bc41-bc5a2c83ab7e</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/Step Definition/Feature 1 - Shinta/Login/LG007 - User login wrong email and password</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>c7eccc46-06e8-4303-a2db-a98c4adde1c8</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/Step Definition/Feature 1 - Shinta/Login/LG006 - User login without input email and password</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>89e9bea2-5369-47d1-80e2-64342d39e87b</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/Step Definition/Feature 1 - Shinta/Login/LG005 - User login without input any password</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>fd5548f7-845f-474e-93a2-96a30e2a316e</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/Step Definition/Feature 1 - Shinta/Login/LG004 - User login without input any email</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>106a4774-e0a8-4a04-9221-f88bd3f419f2</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/Step Definition/Feature 1 - Shinta/Login/LG003 - User login with wrong password</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>8d9ff5d5-6dc2-49eb-a957-d288ceafabca</guid>
+      <guid>a107f5a7-4013-430a-897c-53f356e01b61</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Step Definition/Feature 1 - Shinta/Login/LG002 - User login with unregistered email</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>ee65d194-403d-4310-b958-a212202add66</guid>
+      <guid>7daeffd9-4f9b-447e-a340-f598b936dfbc</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Step Definition/Feature 1 - Shinta/Login/LG001 - User login with correct credential</testCaseId>
+      <testCaseId>Test Cases/Step Definition/Feature 1 - Shinta/Login/LG003 - User login with wrong password</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>8c18043c-9317-4e0d-bcd5-52db1c17a906</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Step Definition/Feature 1 - Shinta/Login/LG004 - User login without input any email</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>75e47bea-8dd5-4461-967e-5e2d892c670e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Step Definition/Feature 1 - Shinta/Login/LG005 - User login without input any password</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>6a9e8391-aa11-44bb-97f9-e03bac795e67</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Step Definition/Feature 1 - Shinta/Login/LG006 - User login without input email and password</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>4e85ad9a-4c25-4d23-a7a9-59fe073b62c9</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Step Definition/Feature 1 - Shinta/Login/LG007 - User login wrong email and password</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>f4e02c33-3b28-4f8b-a0c2-55b5bb9106be</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Step Definition/Feature 1 - Shinta/Login/LG008 - User login without input email and password until user succsessful to login</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
 </TestSuiteEntity>
--- Project WEB/Kelompok_3/Test Suites/Feature 1 - Shinta/Logout.ts
+++ Project WEB/Kelompok_3/Test Suites/Feature 1 - Shinta/Logout.ts
@@ -12,10 +12,17 @@
    <rerunImmediately>false</rerunImmediately>
    <testSuiteGuid>125a0941-06b3-4e24-a771-7383eb235328</testSuiteGuid>
    <testCaseLink>
-      <guid>4c426172-c497-4dbf-8995-fc39c767965d</guid>
+      <guid>c1cd3a4d-8524-4f50-be95-2c4a488e9743</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Step Definition/Feature 1 - Shinta/Logout/LO001 - User logout after login</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>51e2a519-5e30-40de-86b1-b0e11c8b7dfe</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Step Definition/Feature 1 - Shinta/Logout/LO002 - User logout after register</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
 </TestSuiteEntity>
